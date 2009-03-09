f6e8398cbfea5f8c7b21a940562c39fc5495c8d2
emanuele <>
2009-03-09 17:38:57 +0000

    - automatize the calo / track jets - new definition of bkg pdfs

--- src/GenerateZjets.cc
+++ src/GenerateZjets.cc
@@ -1,8 +1,26 @@
 int njets=0;
+Bool_t fitCaloJets=kTRUE;
 
 void SetNjets(int n) { 
   njets=n;
 }
+
+void SetJetFlavour(const char *jetflavour) {
+  if(strcmp(jetflavour,"calojet")==0) fitCaloJets = kTRUE;
+  else fitCaloJets = kFALSE;
+}
+
+// Set Fit Options
+MLOptions GetDefaultOptions() {
+  MLOptions opts;
+  // Fit configuration
+  opts.addBoolOption("fitCaloJets",       "Fit calojets, trackjets otherwise", fitCaloJets);
+  opts.addBoolOption("useMass",           "Use Invariant Mass",     kTRUE);
+  opts.addBoolOption("useMHTphiMET",      "Use sin(MHTphiMET)",     kTRUE);
+
+  return opts;
+}
+
 
 void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0)
 {
@@ -11,6 +29,16 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0)
     cout << "njets not set. Exiting." << endl;
     return;
   }
+
+  // Various fit options...
+  MLOptions opts = GetDefaultOptions();
+  
+  const char* e0 = "\033[44;37m";
+  const char* en="\033[0m";
+  
+  if(opts.getBoolVal("fitCaloJets")) 
+    cout << e0 << "Toy Monte Carlo generation for Z + " << njets << " calojets " << en << endl;
+  else cout << e0 << "Toy Monte Carlo generation for Z + " << njets << " trackjets " << en << endl;
   
   TDatime *now = new TDatime();
   int today = now->GetDate();
@@ -20,7 +48,7 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0)
   RooRandom::randomGenerator()->SetSeed(seed);
 
   // define the structure of the dataset
-  RooRealVar* mass = new RooRealVar("invMass",  "Mass [GeV/c^{2}]" , 60., 110.);
+  RooRealVar* mass = new RooRealVar("mass",  "Mass [GeV/c^{2}]" , 60., 110.);
   RooRealVar* sinMHTphiMET = new RooRealVar("sinMHTphiMET", "sin #phi_{MHT-MET}",-0.85,0.85);
 
   MLFit theFit;
@@ -33,32 +61,41 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0)
   
   // define species
   theFit.addSpecies("myFit", "sig", "Signal Component");
-  theFit.addSpecies("myFit", "ttbarbkg", "Bkg   Component");
+  theFit.addSpecies("myFit", "bkg", "Bkg   Component");
   
   // mLL PDF
-  theFit.addPdfWName("myFit", "sig" , "invMass",  "Cruijff",  "sig_Mass");
-  theFit.addPdfWName("myFit", "ttbarbkg" , "invMass",  "Poly2",  "ttbarbkg_Mass");
-  
+  if(opts.getBoolVal("useMass")) {
+    theFit.addPdfWName("myFit", "sig" , "mass",  "Cruijff",  "sig_Mass");
+    theFit.addPdfWName("myFit", "bkg" , "mass",  "Poly2",    "bkg_Mass");
+  }
+
   // shape variable
-  theFit.addPdfWName("myFit", "sig" , "sinMHTphiMET",  "DoubleGaussian", "sig_sinMHTphiMET");
-  theFit.addPdfWName("myFit", "ttbarbkg" , "sinMHTphiMET",  "Poly2", "ttbarbkg_sinMHTphiMET");
+  if(opts.getBoolVal("useMHTphiMET")) {
+    theFit.addPdfWName("myFit", "sig" , "sinMHTphiMET",  "Cruijff",        "sig_sinMHTphiMET");
+    theFit.addPdfWName("myFit", "bkg" , "sinMHTphiMET",  "Poly2",          "bkg_sinMHTphiMET");
+  }
 
   // build the fit likelihood
   RooAbsPdf *myPdf = theFit.buildModel("myFit");
   
   // Initialize the fit...
+  // Initialize the fit...
+  char jetflavour[200];
+  if(opts.getBoolVal("fitCaloJets")) sprintf(jetflavour, "calojet");
+  else sprintf(jetflavour, "trackjet");
+
   char configfilename[200];
-  sprintf(configfilename, "toyconfig/toyZ-%djet.config",njets);
+  sprintf(configfilename, "toyconfig/toyZ-%d%s.config",njets,jetflavour);
 
   theFit.initialize(configfilename);
   MLGenerator theGenerator(theFit, "myFit");
   
   Int_t ngen =
-    theFit.getRealPar("N_ttbarbkg")->getVal()+
-    theFit.getRealPar("N_sig")->getVal();
+    theFit.getRealPar("N_sig")->getVal()+
+    theFit.getRealPar("N_bkg")->getVal();
 
   // Generate...
-  RooArgSet genVars(theFit.getObsList(MLStrList("invMass","sinMHTphiMET")));
+  RooArgSet genVars(theFit.getObsList(MLStrList("mass","sinMHTphiMET")));
   MLToyStudy theStudy(theGenerator, genVars, "E", "MTE", 0, theFit.getNoNormVars("myFit"));
   theStudy.addFit(*myPdf);
 
