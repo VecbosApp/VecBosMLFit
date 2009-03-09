73b007a30edee1a0312dda97923920fa497a046a
emanuele <>
2009-03-09 21:02:07 +0000

    - new fits with categories for W - new background definition - aumoatic plot generation

--- src/makeplotstoyWjets.cxx
+++ src/makeplotstoyWjets.cxx
@@ -1,3 +1,21 @@
+void makeplotstoyWjetsAll(TString dir, const char* jetflavour) {
+  
+  if(strcmp(jetflavour,"calojet")==0) {  
+    makeplotstoyWjets(dir, "merged-results-W1calojet.dat", "variablesW-1calojet.root", "1jet" );
+    makeplotstoyWjets(dir, "merged-results-W2calojet.dat", "variablesW-2calojet.root", "2jet" );
+    makeplotstoyWjets(dir, "merged-results-W3calojet.dat", "variablesW-3calojet.root", "3jet" );
+    makeplotstoyWjets(dir, "merged-results-W4calojet.dat", "variablesW-4calojet.root", "4jet" );
+  } else if(strcmp(jetflavour,"trackjet")==0) {
+    makeplotstoyWjets(dir, "merged-results-W1trackjet.dat", "variablesW-1trackjet.root", "1jet" );
+    makeplotstoyWjets(dir, "merged-results-W2trackjet.dat", "variablesW-2trackjet.root", "2jet" );
+    makeplotstoyWjets(dir, "merged-results-W3trackjet.dat", "variablesW-3trackjet.root", "3jet" );
+    makeplotstoyWjets(dir, "merged-results-W4trackjet.dat", "variablesW-4trackjet.root", "4jet" );
+  } else {
+    cout << "unknown " << jetflavour << ". Exiting." << endl;
+    return;
+  }
+}
+
 // macor to make usual plots from a set of Toys
 void makeplotstoyWjets(TString dir = "./toys", TString file = "resultsW-1jet.dat", 
 		       TString varfile = "variablesWjets.root", TString suffix = "1jet") {
@@ -7,13 +25,22 @@ void makeplotstoyWjets(TString dir = "./toys", TString file = "resultsW-1jet.dat
   gStyle->SetOptTitle(0);
 
   TH1D* N_sig_pull = new TH1D("N_sig_pull"," ",20, -4., 4.);
-  TH1D* N_sig_err = new TH1D("N_sig_err"," ",50, 100, 300.);
+  TH1D* N_sig_err = new TH1D("N_sig_err"," ",50, 350, 500.);
 
-  TH1D* N_zeeBkg_pull = new TH1D("N_zeeBkg_pull"," ",20, -4., 4.);
-  TH1D* N_zeeBkg_err = new TH1D("N_zeeBkg_err"," ",400, 0., 150.);
+  TH1D* N_eff_sig_pull = new TH1D("N_eff_sig_pull"," ",20, -4., 4.);
+  TH1D* N_eff_sig_err = new TH1D("N_eff_sig_err"," ",50, 350, 500.);
 
-  TH1D* N_ttbarBkg_pull = new TH1D("N_ttbarBkg_pull"," ",20, -4., 4.);
-  TH1D* N_ttbarBkg_err = new TH1D("N_ttbarBkg_err"," ",400, 0., 150.);
+  TH1D* N_ttbar_pull = new TH1D("N_ttbar_pull"," ",20, -4., 4.);
+  TH1D* N_ttbar_err = new TH1D("N_ttbar_err"," ",400, 0., 150.);
+
+  TH1D* N_eff_ttbar_pull = new TH1D("N_eff_ttbar_pull"," ",20, -4., 4.);
+  TH1D* N_eff_ttbar_err = new TH1D("N_eff_ttbar_err"," ",400, 0., 150.);
+
+  TH1D* N_other_pull = new TH1D("N_other_pull"," ",20, -4., 4.);
+  TH1D* N_other_err = new TH1D("N_other_err"," ",400, 0., 150.);
+
+  TH1D* N_eff_other_pull = new TH1D("N_eff_other_pull"," ",20, -4., 4.);
+  TH1D* N_eff_other_err = new TH1D("N_eff_other_err"," ",400, 0., 150.);
 
   gStyle->SetOptFit(1111111);
   TTree* ntp = readtoy(dir,file,varfile);
@@ -21,11 +48,21 @@ void makeplotstoyWjets(TString dir = "./toys", TString file = "resultsW-1jet.dat
   ntp->Project("N_sig_pull","(N_sig - N_siggen)/N_sigerr","covQual==3");
   ntp->Project("N_sig_err","N_sigerr","covQual==3");
 
-  ntp->Project("N_zeeBkg_pull","(N_zeeBkg - N_zeeBkggen)/N_zeeBkgerr","covQual==3");
-  ntp->Project("N_zeeBkg_err","N_zeeBkgerr","covQual==3");
+  ntp->Project("N_eff_sig_pull","(N_eff_sig - N_eff_siggen)/N_eff_sigerr","covQual==3");
+  ntp->Project("N_eff_sig_err","N_eff_sigerr","covQual==3");
 
-  ntp->Project("N_ttbarBkg_pull","(N_ttbarBkg - N_ttbarBkggen)/N_ttbarBkgerr","covQual==3");
-  ntp->Project("N_ttbarBkg_err","N_ttbarBkgerr","covQual==3");
+  ntp->Project("N_ttbar_pull","(N_ttbar - N_ttbargen)/N_ttbarerr","covQual==3");
+  ntp->Project("N_ttbar_err","N_ttbarerr","covQual==3");
+
+  ntp->Project("N_eff_ttbar_pull","(N_eff_ttbar - N_eff_ttbargen)/N_eff_ttbarerr","covQual==3");
+  ntp->Project("N_eff_ttbar_err","N_eff_ttbarerr","covQual==3");
+
+  ntp->Project("N_other_pull","(N_other - N_othergen)/N_othererr","covQual==3");
+  ntp->Project("N_other_err","N_othererr","covQual==3");
+
+  ntp->Project("N_eff_other_pull","(N_eff_other - N_eff_othergen)/N_eff_othererr","covQual==3");
+  ntp->Project("N_eff_other_err","N_eff_othererr","covQual==3");
+
  
   // pull distributions, fitted as Gaussians
   TCanvas* c1 = new TCanvas("c1","c1",500,500);
@@ -39,24 +76,45 @@ void makeplotstoyWjets(TString dir = "./toys", TString file = "resultsW-1jet.dat
 
   N_sig_pull->Fit("gaus");
   N_sig_pull->GetXaxis()->SetTitle("Pull N_{sig}");
-  TString pullstring_sig("toy-plots/Wjets/N_sig_pull_");
+  TString pullstring_sig(dir+"/N_sig_pull_");
   pullstring_sig.Append(suffix);
   pullstring_sig.Append(".eps");
   c1->SaveAs(pullstring_sig);
 
-  N_zeeBkg_pull->Fit("gaus");
-  N_zeeBkg_pull->GetXaxis()->SetTitle("Pull N_{Z bkg}");
-  TString pullstring_zeeBkg("toy-plots/Wjets/N_zeeBkg_pull_");
-  pullstring_zeeBkg.Append(suffix);
-  pullstring_zeeBkg.Append(".eps");
-  c1->SaveAs(pullstring_zeeBkg);
+  N_eff_sig_pull->Fit("gaus");
+  N_eff_sig_pull->GetXaxis()->SetTitle("Pull N_{eff_sig}");
+  TString pullstring_eff_sig(dir+"/N_eff_sig_pull_");
+  pullstring_eff_sig.Append(suffix);
+  pullstring_eff_sig.Append(".eps");
+  c1->SaveAs(pullstring_eff_sig);
 
-  N_ttbarBkg_pull->Fit("gaus");
-  N_ttbarBkg_pull->GetXaxis()->SetTitle("Pull N_{tt bkg}");
-  TString pullstring_ttbarBkg("toy-plots/Wjets/N_ttbarBkg_pull_");
-  pullstring_ttbarBkg.Append(suffix);
-  pullstring_ttbarBkg.Append(".eps");
-  c1->SaveAs(pullstring_ttbarBkg);
+  N_ttbar_pull->Fit("gaus");
+  N_ttbar_pull->GetXaxis()->SetTitle("Pull N_{tt bkg}");
+  TString pullstring_ttbar(dir+"/N_ttbar_pull_");
+  pullstring_ttbar.Append(suffix);
+  pullstring_ttbar.Append(".eps");
+  c1->SaveAs(pullstring_ttbar);
+
+  N_eff_ttbar_pull->Fit("gaus");
+  N_eff_ttbar_pull->GetXaxis()->SetTitle("Pull N_{tt bkg}");
+  TString pullstring_eff_ttbar(dir+"/N_eff_ttbar_pull_");
+  pullstring_eff_ttbar.Append(suffix);
+  pullstring_eff_ttbar.Append(".eps");
+  c1->SaveAs(pullstring_eff_ttbar);
+
+  N_other_pull->Fit("gaus");
+  N_other_pull->GetXaxis()->SetTitle("Pull N_{other}");
+  TString pullstring_other(dir+"/N_other_pull_");
+  pullstring_other.Append(suffix);
+  pullstring_other.Append(".eps");
+  c1->SaveAs(pullstring_other);
+
+  N_eff_other_pull->Fit("gaus");
+  N_eff_other_pull->GetXaxis()->SetTitle("Pull N_{eff_other}");
+  TString pullstring_eff_other(dir+"/N_eff_other_pull_");
+  pullstring_eff_other.Append(suffix);
+  pullstring_eff_other.Append(".eps");
+  c1->SaveAs(pullstring_eff_other);
 
   // error distributions
   TCanvas* c2 = new TCanvas("c2","c2",500,500);
@@ -70,24 +128,45 @@ void makeplotstoyWjets(TString dir = "./toys", TString file = "resultsW-1jet.dat
 
   N_sig_err->GetXaxis()->SetTitle("#sigma(N_{sig})");
   N_sig_err->Draw();
-  TString errstring_sig("toy-plots/Wjets/N_sig_err_");
+  TString errstring_sig(dir+"/Wjets/N_sig_err_");
   errstring_sig.Append(suffix);
   errstring_sig.Append(".eps");
   c2->SaveAs(errstring_sig);
 
-  N_zeeBkg_err->GetXaxis()->SetTitle("#sigma(N_{Z bkg})");
-  N_zeeBkg_err->Draw();
-  TString errstring_zeeBkg("toy-plots/Wjets/N_zeeBkg_err_");
-  errstring_zeeBkg.Append(suffix);
-  errstring_zeeBkg.Append(".eps");
-  c2->SaveAs(errstring_zeeBkg);
+  N_eff_sig_err->GetXaxis()->SetTitle("#eff_sigma(N_{eff_sig})");
+  N_eff_sig_err->Draw();
+  TString errstring_eff_sig(dir+"/Wjets/N_eff_sig_err_");
+  errstring_eff_sig.Append(suffix);
+  errstring_eff_sig.Append(".eps");
+  c2->SaveAs(errstring_eff_sig);
 
-  N_ttbarBkg_err->GetXaxis()->SetTitle("#sigma(N_{tt bkg})");
-  N_ttbarBkg_err->Draw();
-  TString errstring_ttbarBkg("toy-plots/Wjets/N_ttbarBkg_err_");
-  errstring_ttbarBkg.Append(suffix);
-  errstring_ttbarBkg.Append(".eps");
-  c2->SaveAs(errstring_ttbarBkg);
+  N_ttbar_err->GetXaxis()->SetTitle("#sigma(N_{tt})");
+  N_ttbar_err->Draw();
+  TString errstring_ttbar(dir+"/N_ttbar_err_");
+  errstring_ttbar.Append(suffix);
+  errstring_ttbar.Append(".eps");
+  c2->SaveAs(errstring_ttbar);
+
+  N_eff_ttbar_err->GetXaxis()->SetTitle("#sigma(N_{tt})");
+  N_eff_ttbar_err->Draw();
+  TString errstring_eff_ttbar(dir+"/N_eff_ttbar_err_");
+  errstring_eff_ttbar.Append(suffix);
+  errstring_eff_ttbar.Append(".eps");
+  c2->SaveAs(errstring_eff_ttbar);
+
+  N_other_err->GetXaxis()->SetTitle("#sigma(N_{tt})");
+  N_other_err->Draw();
+  TString errstring_other(dir+"/N_other_err_");
+  errstring_other.Append(suffix);
+  errstring_other.Append(".eps");
+  c2->SaveAs(errstring_other);
+
+  N_eff_other_err->GetXaxis()->SetTitle("#sigma(N_{tt})");
+  N_eff_other_err->Draw();
+  TString errstring_eff_other(dir+"/N_eff_other_err_");
+  errstring_eff_other.Append(suffix);
+  errstring_eff_other.Append(".eps");
+  c2->SaveAs(errstring_eff_other);
 
 }
 
--- src/makeplotstoyZjets.cxx
+++ src/makeplotstoyZjets.cxx
@@ -1,3 +1,22 @@
+void makeplotstoyZjetsAll(TString dir, const char* jetflavour) {
+  
+  if(strcmp(jetflavour,"calojet")==0) {  
+    makeplotstoyZjets(dir, "merged-results-Z1calojet.dat", "variablesZ-1calojet.root", "1jet" );
+    makeplotstoyZjets(dir, "merged-results-Z2calojet.dat", "variablesZ-2calojet.root", "2jet" );
+    makeplotstoyZjets(dir, "merged-results-Z3calojet.dat", "variablesZ-3calojet.root", "3jet" );
+    makeplotstoyZjets(dir, "merged-results-Z4calojet.dat", "variablesZ-4calojet.root", "4jet" );
+  } else if(strcmp(jetflavour,"trackjet")==0) {
+    makeplotstoyZjets(dir, "merged-results-Z1trackjet.dat", "variablesZ-1trackjet.root", "1jet" );
+    makeplotstoyZjets(dir, "merged-results-Z2trackjet.dat", "variablesZ-2trackjet.root", "2jet" );
+    makeplotstoyZjets(dir, "merged-results-Z3trackjet.dat", "variablesZ-3trackjet.root", "3jet" );
+    makeplotstoyZjets(dir, "merged-results-Z4trackjet.dat", "variablesZ-4trackjet.root", "4jet" );
+  } else {
+    cout << "unknown " << jetflavour << ". Exiting." << endl;
+    return;
+  }
+}
+
+
 // macor to make usual plots from a set of Toys
 void makeplotstoyZjets(TString dir = "./toys", TString file = "resultsZ-1jet.dat", 
 		       TString varfile = "variablesZjets.root", TString suffix = "1jet") {
@@ -9,11 +28,17 @@ void makeplotstoyZjets(TString dir = "./toys", TString file = "resultsZ-1jet.dat
   TH1D* N_sig_pull = new TH1D("N_sig_pull"," ",20, -4., 4.);
   TH1D* N_sig_err = new TH1D("N_sig_err"," ",30, 0., 20.);
 
+  TH1D* N_bkg_pull = new TH1D("N_bkg_pull"," ",20, -4., 4.);
+  TH1D* N_bkg_err = new TH1D("N_bkg_err"," ",30, 0., 20.);
+
   gStyle->SetOptFit(1111111);
   TTree* ntp = readtoy(dir,file,varfile);
 
   ntp->Project("N_sig_pull","(N_sig - N_siggen)/N_sigerr","covQual==3");
   ntp->Project("N_sig_err","N_sigerr","covQual==3");
+
+  ntp->Project("N_bkg_pull","(N_bkg - N_bkggen)/N_bkgerr","covQual==3");
+  ntp->Project("N_bkg_err","N_bkgerr","covQual==3");
 
  
   // pull distributions, fitted as Gaussians
@@ -25,10 +50,17 @@ void makeplotstoyZjets(TString dir = "./toys", TString file = "resultsZ-1jet.dat
   c1->SetFrameBorderMode(0);
   gStyle->SetOptFit(1);
   gStyle->SetOptStat(0);
+
   N_sig_pull->Fit("gaus");
   N_sig_pull->GetXaxis()->SetTitle("Pull N_{sig}");
+  TString pullstring(dir+"/N_sig_pull_");
+  pullstring.Append(suffix);
+  pullstring.Append(".eps");
+  c1->SaveAs(pullstring);
 
-  TString pullstring("toy-plots/Zjets/N_sig_pull_");
+  N_bkg_pull->Fit("gaus");
+  N_bkg_pull->GetXaxis()->SetTitle("Pull N_{bkg}");
+  TString pullstring(dir+"/N_bkg_pull_");
   pullstring.Append(suffix);
   pullstring.Append(".eps");
   c1->SaveAs(pullstring);
@@ -42,10 +74,17 @@ void makeplotstoyZjets(TString dir = "./toys", TString file = "resultsZ-1jet.dat
   c2->SetFrameBorderMode(0);
   gStyle->SetOptFit(0);
   gStyle->SetOptStat(1111111);
+
   N_sig_err->GetXaxis()->SetTitle("#sigma(N_{sig})");
   N_sig_err->Draw();
+  TString errstring(dir+"/N_sig_err_");
+  errstring.Append(suffix);
+  errstring.Append(".eps");
+  c2->SaveAs(errstring);
 
-  TString errstring("toy-plots/Zjets/N_sig_err_");
+  N_bkg_err->GetXaxis()->SetTitle("#sigma(N_{bkg})");
+  N_bkg_err->Draw();
+  TString errstring(dir+"/N_bkg_err_");
   errstring.Append(suffix);
   errstring.Append(".eps");
   c2->SaveAs(errstring);
