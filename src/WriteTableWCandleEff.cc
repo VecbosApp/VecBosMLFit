3ec651bcde2b7dff6636fd9be80c0e4cb496edff
emanuele <>
2010-07-12 13:05:51 +0000

    - enhance the cut on MET/pt (now no convrej applied) - write the table in tex-like format

--- src/WriteTableWCandleEff.cc
+++ src/WriteTableWCandleEff.cc
@@ -1,9 +1,12 @@
+#include <iostream>
 #include <vector>
 #include <TString.h>
 #include <TFile.h>
 #include <RooFitResult.h>
 #include <RooRealVar.h>
 #include <RooArgList.h>
+
+using namespace std;
 
 void writeTable(const char *dir, const char *ana = "Id") {
 
@@ -12,16 +15,16 @@ void writeTable(const char *dir, const char *ana = "Id") {
 
   WPs.push_back("WP70");
   WPs.push_back("WP80");
-  WPs.push_back("WP80modEE");
+  WPs.push_back("WP90");
   WPs.push_back("WP95");
 
-  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.3_AND_absOPPARetaCLPARLT1.479"); // EB
-  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.3_AND_absOPPARetaCLPARLT1.479_AND_etaGT0"); // EB+
-  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.3_AND_absOPPARetaCLPARLT1.479_AND_etaLT0"); // EB-
+  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.4_AND_absOPPARetaCLPARLT1.479"); // EB
+  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.4_AND_absOPPARetaCLPARLT1.479_AND_etaGT0"); // EB+
+  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.4_AND_absOPPARetaCLPARLT1.479_AND_etaLT0"); // EB-
 
-  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.3_AND_absOPPARetaCLPARGT1.479"); // EE
-  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.3_AND_absOPPARetaCLPARGT1.479_AND_etaGT0"); // EE+
-  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.3_AND_absOPPARetaCLPARGT1.479_AND_etaLT0"); // EE-
+  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.4_AND_absOPPARetaCLPARGT1.479"); // EE
+  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.4_AND_absOPPARetaCLPARGT1.479_AND_etaGT0"); // EE+
+  cuts.push_back("nJetsEQ0_AND_pfmetOVERptGT0.4_AND_absOPPARetaCLPARGT1.479_AND_etaLT0"); // EE-
   
   std::vector<string> idCuts;
   idCuts.push_back("EB");
@@ -46,20 +49,24 @@ void writeTable(const char *dir, const char *ana = "Id") {
       filename += TString("is")+TString(ana);
       filename += WPs[iwp];
       filename += ".root";
-      
+    
       TFile *file = TFile::Open(filename.Data());
-      RooFitResult *fitres = (RooFitResult*)file->Get("nll_myFit_T1");
+      RooFitResult *fitres = (RooFitResult*)file->Get("fitresult_myFit_T1");
       RooArgList floatPars = fitres->floatParsFinal();
       RooRealVar *N_sig = (RooRealVar*)floatPars.find("N_sig");
       RooRealVar *N_qcd = (RooRealVar*)floatPars.find("N_qcd");
       RooRealVar *eff_sig = (RooRealVar*)floatPars.find("eff_sig");
       RooRealVar *eff_qcd = (RooRealVar*)floatPars.find("eff_qcd");
-      
-      cout << idCuts[icut] 
-           << "\t" << N_sig->getVal() << " +/- " << N_sig->getError()
-           << "\t" << eff_sig->getVal() << " +/- " << eff_sig->getError() 
-           << "\t" << N_qcd->getVal() << " +/- " << N_qcd->getError() 
-           << "\t" << eff_qcd->getVal() << " +/- " << eff_qcd->getError() << endl;
+
+      cout.setf(ios::fixed,ios::floatfield); 
+      cout.precision(3);
+      cout << idCuts[icut] << " & " 
+           << "\t" << N_sig->getVal() << " $\\pm$ " << N_sig->getError() << " & "
+           << "\t" << eff_sig->getVal() << " $^{+" << eff_sig->getAsymErrorHi() << "}_{" << eff_sig->getAsymErrorLo() << "}$ & " 
+           << "\t" << N_qcd->getVal() << " $\\pm$ " << N_qcd->getError() << " & " 
+           << "\t" << eff_qcd->getVal() << " $\\pm$ " << eff_qcd->getError()
+           << " \\\\" << endl;
+
     }
   }
 
