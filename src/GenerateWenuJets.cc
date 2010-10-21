475b01eee5755ad5ad6924725d7c1109b38a0ef9
emanuele <>
2010-10-21 09:14:29 +0000

    add the btag variables

--- src/GenerateWenuJets.cc
+++ src/GenerateWenuJets.cc
@@ -5,6 +5,7 @@ MLOptions GetDefaultOptions() {
   opts.addBoolOption("highJetThreshold", "Fit W+jets with ET>30 GeV", kTRUE);
   opts.addBoolOption("fitRatio",         "FitRatio directly",kFALSE);
   opts.addBoolOption("usePfMt",         "Use W Transverse Mass",  kTRUE);
+  opts.addBoolOption("useBTag",         "Use B Tag",  kTRUE);
   opts.addRealOption("njetmin",         "smallest jet number to consider", 1);
   opts.addRealOption("njetmax",         "largest jet number to consider", 4);
 
@@ -30,23 +31,32 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
 
   // define the structure of the dataset
   RooRealVar* pfmt = new RooRealVar("pfmt",  "M_{T}" , 20., 150., "GeV/c^{2}");
+  RooRealVar* btag = new RooRealVar("combinedSecondaryVertexBJetTags", "EVT b-tag", 0., 1.);
   RooRealVar *njets = new RooRealVar("nJets", "nJets",0.5, 20.5);
 
   theFit.AddFlatFileColumn(pfmt);
+  theFit.AddFlatFileColumn(btag);
   theFit.AddFlatFileColumn(njets);
 
   // define a fit model
   theFit.addModel("myFit", "Ratio WtoENu");
 
   // define species
-  MLStrList speclist;
+  MLStrList speclistLF, speclistHF;
   for(int nj = opts.getRealVal("njetmin"); nj <=opts.getRealVal("njetmax"); nj++){
-    //signal
+    //signal LF
     char speclabel[50],specdesc[200];
-    sprintf(speclabel,"W%dj",nj);
-    sprintf(specdesc,"Signal %dj Component",nj);
+    sprintf(speclabel,"W_LF%dj",nj);
+    sprintf(specdesc,"Signal LF %dj Component",nj);
     theFit.addSpecies("myFit", speclabel, specdesc);
-    speclist.Add(speclabel); // exclusive species list ot be transformed in inclusive.
+    speclistLF.Add(speclabel); // exclusive species list ot be transformed in inclusive.
+
+    //signal HF
+    char speclabel[50],specdesc[200];
+    sprintf(speclabel,"W_HF%dj",nj);
+    sprintf(specdesc,"Signal HF %dj Component",nj);
+    theFit.addSpecies("myFit", speclabel, specdesc);
+    speclistHF.Add(speclabel); // exclusive species list ot be transformed in inclusive.
 
     //QCD
     sprintf(speclabel,"qcd%dj",nj);
@@ -59,20 +69,32 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
 
   }
  
-  if(opts.getBoolVal("fitRatio"))  theFit.fitInclusiveRatioPoly(speclist, "Wincl",3);
-  else  theFit.fitInclusive( speclist, "Wincl",1);
+  if(opts.getBoolVal("fitRatio")) {
+    theFit.fitInclusiveRatioPoly(speclistLF, "WinclLF",1);
+    theFit.fitInclusiveRatioPoly(speclistHF, "WinclHF",1);
+  }
+  else {
+    theFit.fitInclusive( speclistLF, "WinclLF",1);
+    theFit.fitInclusive( speclistHF, "WinclHF",1);
+  }
 
   for(int nj = opts.getRealVal("njetmin"); nj <=opts.getRealVal("njetmax"); nj++){
 
-    char speclabel[50],pdfname[200];
+    char speclabel[50],pdfname[200],specnameToCopy[200];
 
     // Mt PDF
     if(opts.getBoolVal("usePfMt")) {
       
-      //signal
-      sprintf(speclabel,"W%dj",nj);
-      sprintf(pdfname,"sig_PfMt%dj",nj);   
+      //signal LF
+      sprintf(speclabel,"W_LF%dj",nj);
+      sprintf(pdfname,"sigLF_PfMt%dj",nj);
       theFit.addPdfWName("myFit", speclabel , "pfmt",  "DoubleCruijff", pdfname );
+
+      //signal HF
+      sprintf(speclabel,"W_HF%dj",nj);
+      sprintf(pdfname,"sigHF_PfMt%dj",nj);
+      sprintf(specnameToCopy,"W_LF%dj",nj);
+      theFit.addPdfCopy("myFit", speclabel , "pfmt",  specnameToCopy ); // same as W+LF
 
       //QCD
       sprintf(speclabel,"qcd%dj",nj);
@@ -95,10 +117,52 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
       } else theFit.addPdfWName("myFit",speclabel  , "pfmt",  "Cruijff",   pdfname);
     }
 
+    // EVT b-tag 
+    if(opts.getBoolVal("useBTag")) {
+
+      // used only in the 1/2-jet bin
+      const int nbins = 10;
+      double limitarray[] = {0.0, 0.1, 0.2, 0.3, 0.6, 0.7, 0.8, 0.85, 0.9, 0.95, 1.0 };
+      double limitarrayOther[] = {0.0, 0.15, 0.25, 0.45, 0.80, 0.84, 0.86, 0.90, 0.93, 0.96, 1.0001 };
+
+      TAxis* limits = new TAxis(nbins,limitarray) ;
+      TList args;
+      args.Add(limits);
+
+      TAxis* limitsOther = new TAxis(nbins,limitarrayOther) ;
+      TList argsOther;
+      argsOther.Add(limitsOther);
+
+      //other
+      sprintf(speclabel,"other%dj",nj);
+      sprintf(pdfname,"other_btag%dj",nj);   
+      theFit.addPdfWName("myFit", speclabel , "combinedSecondaryVertexBJetTags",  "BinnedPdf", argsOther, pdfname );
+      
+      // signal LF
+      sprintf(speclabel,"W_LF%dj",nj);
+      sprintf(pdfname,"sigLF_btag%dj",nj);   
+      theFit.addPdfWName("myFit", speclabel , "combinedSecondaryVertexBJetTags",  "BinnedPdf", args, pdfname );
+
+      // signal HF
+      sprintf(speclabel,"W_HF%dj",nj);
+      sprintf(pdfname,"sigHF_btag%dj",nj);   
+      sprintf(specnameToCopy,"other%dj",nj); 
+      theFit.addPdfCopy("myFit", speclabel, "combinedSecondaryVertexBJetTags", specnameToCopy );
+
+      //QCD
+      sprintf(speclabel,"qcd%dj",nj);
+      sprintf(pdfname,"qcd_btag%dj",nj);   
+      theFit.addPdfWName("myFit", speclabel , "combinedSecondaryVertexBJetTags",  "BinnedPdf", args, pdfname );
+
+    }
+
     // jet category
     sprintf(pdfname,"%dj",nj);
 
-    sprintf(speclabel,"W%dj",nj);
+    sprintf(speclabel,"W_LF%dj",nj);
+    theFit.addPdfWName("myFit",speclabel , "nJets", "ExclJet", pdfname);
+
+    sprintf(speclabel,"W_HF%dj",nj);
     theFit.addPdfWName("myFit",speclabel , "nJets", "ExclJet", pdfname);
 
     sprintf(speclabel,"qcd%dj",nj);
@@ -108,8 +172,6 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
     theFit.addPdfWName("myFit",speclabel , "nJets", "ExclJet", pdfname);
 
   }
-
-  cout << "stocase6" << endl;
 
 
   // build the fit likelihood
@@ -131,7 +193,8 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
 
 
   Int_t ngen =
-    theFit.getRealPar("N_Wincl1j")->getVal()+
+    theFit.getRealPar("N_WinclLF1j")->getVal()+
+    theFit.getRealPar("N_WinclHF1j")->getVal()+
     theFit.getRealPar("N_qcd1j")->getVal()+
     theFit.getRealPar("N_qcd2j")->getVal()+
     theFit.getRealPar("N_qcd3j")->getVal()+
@@ -142,7 +205,7 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
     theFit.getRealPar("N_other4j")->getVal();
 
   // Generate...
-  RooArgSet genVars(theFit.getObsList(MLStrList("pfmt","nJets")));
+  RooArgSet genVars(theFit.getObsList(MLStrList("pfmt","combinedSecondaryVertexBJetTags","nJets")));
   MLToyStudy theStudy(theGenerator, genVars, "E", "MTE", 0, theFit.getNoNormVars("myFit"));
   theStudy.addFit(*myPdf);
 
