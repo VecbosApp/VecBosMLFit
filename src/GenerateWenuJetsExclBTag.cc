3464fb701956d3efcdb4a538009ad480887fad41
emanuele <>
2011-01-26 15:58:28 +0000

    - add the code for systematics - add the possibility to generate exclusive signal

--- src/GenerateWenuJetsExclBTag.cc
+++ src/GenerateWenuJetsExclBTag.cc
@@ -6,14 +6,28 @@ MLOptions GetDefaultOptions() {
   opts.addBoolOption("fitRatio",         "FitRatio directly",kFALSE);
   opts.addBoolOption("usePfMt",          "Use W Transverse Mass",  kTRUE);
   opts.addBoolOption("useBTag",          "Use B Tag",  kTRUE);
+  opts.addBoolOption("fitInclusive",     "Fit inclusive W+jets multiplicity", kFALSE);
   opts.addRealOption("njetmin",          "smallest jet number to consider", 1);
   opts.addRealOption("njetmax",          "largest jet number to consider", 4);
   opts.addBoolOption("MCT",              "do an embedded toy", kFALSE);
+  opts.addBoolOption("systematics",      "do systematics estimation", kFALSE);
+  opts.addBoolOption("smearBTagEff",     "apply unfiform variation of the btag efficiency", kFALSE);
+  opts.addBoolOption("smearBMistag",     "apply unfiform variation of the btag mistag rate", kTRUE);
+  opts.addBoolOption("smearSig3j",       "apply unfiform variation of the signal 3j shape", kFALSE);
+  opts.addBoolOption("smearSig4j",       "apply unfiform variation of the signal 4j shape", kFALSE);
+  opts.addBoolOption("smearTop0j",       "apply unfiform variation of the top 0j shape", kFALSE);
+  opts.addBoolOption("smearTop1j",       "apply unfiform variation of the top 1j shape", kFALSE);
+  opts.addBoolOption("smearTop2j",       "apply unfiform variation of the top 2j shape", kFALSE);
+  opts.addBoolOption("smearTop3j",       "apply unfiform variation of the top 3j shape", kFALSE);
+  opts.addBoolOption("smearTop4j",       "apply unfiform variation of the top 4j shape", kFALSE);
   return opts;
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MLFit theFit;
+MLFit *theFit2[10000];
 
 void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
 
@@ -24,7 +38,6 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
   // Set the random number seed...
   RooRandom::randomGenerator()->SetSeed(seed);
 
-  MLFit theFit;
 
   // Various fit options...
   MLOptions opts = GetDefaultOptions();
@@ -36,7 +49,7 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
 
   theFit.AddFlatFileColumn(pfmt);
   theFit.AddFlatFileColumn(nbj);
-  theFit.AddFlatFileColumn(njets);
+  theFit.AddFlatFileColumn(njetsHi);
 
   // define a fit model
   theFit.addModel("myFit", "Ratio WtoENu");
@@ -76,7 +89,7 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
     theFit.fitInclusiveRatio(speclist, "Wincl",opts.getRealVal("njetmin"));
   }
   else { 
-    theFit.fitInclusive( speclist, "Wincl_",opts.getRealVal("njetmin"));
+    if(opts.getBoolVal("fitInclusive")) theFit.fitInclusive( speclist, "Wincl_",opts.getRealVal("njetmin"));
   }
 
   char speclabel[50],pdfname[200],speclabelToCopy[200];
@@ -158,26 +171,6 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
       theFit.bind(MLStrList(effb0,effb1,effb2),"btag_eb","btag_eb");
       theFit.bind(MLStrList(effnob0,effnob1,effnob2),"btag_enob","btag_enob");
 
-//       sprintf(effb0,"sig_b0_btag_%dj_eb",nj);
-//       sprintf(effb1,"sig_b1_btag_%dj_eb",nj);
-//       sprintf(effb2,"sig_b2_btag_%dj_eb",nj);
-//       sprintf(effnob0,"sig_b0_btag_%dj_enob",nj);
-//       sprintf(effnob1,"sig_b1_btag_%dj_enob",nj);
-//       sprintf(effnob2,"sig_b2_btag_%dj_enob",nj);
-
-//       theFit.bind(MLStrList(effb0,effb1,effb2),"sig_btag_eb","sig_btag_eb");
-//       theFit.bind(MLStrList(effnob0,effnob1,effnob2),"sig_btag_enob","sig_btag_enob");
-
-//       sprintf(effb0,"top_b0_btag_%dj_eb",nj);
-//       sprintf(effb1,"top_b1_btag_%dj_eb",nj);
-//       sprintf(effb2,"top_b2_btag_%dj_eb",nj);
-//       sprintf(effnob0,"top_b0_btag_%dj_enob",nj);
-//       sprintf(effnob1,"top_b1_btag_%dj_enob",nj);
-//       sprintf(effnob2,"top_b2_btag_%dj_enob",nj);
-
-//       theFit.bind(MLStrList(effb0,effb1,effb2),"top_btag_eb","top_btag_eb");
-//       theFit.bind(MLStrList(effnob0,effnob1,effnob2),"top_btag_enob","top_btag_enob");
- 
     }
     
   
@@ -222,8 +215,15 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
   Int_t ngen = 0;
   if(opts.getBoolVal("fitRatio")) 
     ngen = theFit.getRealPar("N_Wincl_total")->getVal();
-  else
-    ngen = theFit.getRealPar("N_Wincl_1j")->getVal();
+  else {
+    if(opts.getBoolVal("fitInclusive")) ngen = theFit.getRealPar("N_Wincl_1j")->getVal();
+    else {
+      ngen = theFit.getRealPar("N_W_1j")->getVal()+
+        theFit.getRealPar("N_W_2j")->getVal()+
+        theFit.getRealPar("N_W_3j")->getVal()+
+        theFit.getRealPar("N_W_4j")->getVal();
+    }
+  }
 
   ngen += theFit.getRealPar("N_other_1j")->getVal()+theFit.getRealPar("N_other_2j")->getVal()+theFit.getRealPar("N_other_3j")->getVal()+theFit.getRealPar("N_other_4j")->getVal()+
     theFit.getRealPar("N_top0b_1j")->getVal()+theFit.getRealPar("N_top0b_2j")->getVal()+theFit.getRealPar("N_top0b_3j")->getVal()+theFit.getRealPar("N_top0b_4j")->getVal()+
@@ -244,11 +244,74 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
   }
   theStudy.addFit(*myPdf);
 
+  // systematics
+  if(opts.getBoolVal("systematics")) {
+    for(int ifit=0; ifit<1; ifit++) {
+      
+      RooAbsPdf *myPdf2 = 0;
+      
+      char fitname[200];
+      sprintf(fitname,"myFit%d",ifit);
+      
+      getSecondFit(ifit);
+      myPdf2 = theFit2[ifit]->buildModel(fitname);
+      
+      //initialize the second fit...
+      theFit2[ifit]->initialize(initconfigfile);
+      
+      // smear group of correlated parameters for systematics 
+      if(opts.getBoolVal("smearBTagEff")) {
+        MLStrList params("btag_eb");
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearBMistag")) {
+        MLStrList params("btag_enob");
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearSig3j")) {
+        MLStrList SigMTParams1("sig_PfMt3j_mean1","sig_PfMt3j_sigmaL1","sig_PfMt3j_sigmaR1","sig_PfMt3j_alphaL1","sig_PfMt3j_alphaR1");
+        MLStrList SigMTParams2("sig_PfMt3j_mean2","sig_PfMt3j_sigmaL2","sig_PfMt3j_sigmaR2","sig_PfMt3j_alphaL2","sig_PfMt3j_alphaR2","sig_PfMt3j_f1");
+        MLStrList params(SigMTParams1);
+        params.Add(SigMTParams2);
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearSig4j")) {
+        MLStrList SigMTParams1("sig_PfMt4j_mean1","sig_PfMt4j_sigmaL1","sig_PfMt4j_sigmaR1","sig_PfMt4j_alphaL1","sig_PfMt4j_alphaR1");
+        MLStrList SigMTParams2("sig_PfMt4j_mean2","sig_PfMt4j_sigmaL2","sig_PfMt4j_sigmaR2","sig_PfMt4j_alphaL2","sig_PfMt4j_alphaR2","sig_PfMt4j_f1");
+        MLStrList params(SigMTParams1);
+        params.Add(SigMTParams2);
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearTop0j")) {
+        MLStrList params("top_PfMt0j_mean","top_PfMt0j_sigmaL","top_PfMt0j_sigmaR","top_PfMt0j_alphaL","top_PfMt0j_alphaR");
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearTop1j")) {
+        MLStrList params("top_PfMt1j_mean","top_PfMt1j_sigmaL","top_PfMt1j_sigmaR","top_PfMt1j_alphaL","top_PfMt1j_alphaR");
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearTop2j")) {
+        MLStrList params("top_PfMt2j_mean","top_PfMt2j_sigmaL","top_PfMt2j_sigmaR","top_PfMt2j_alphaL","top_PfMt2j_alphaR");
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearTop3j")) {
+        MLStrList params("top_PfMt3j_mean","top_PfMt3j_sigmaL","top_PfMt3j_sigmaR","top_PfMt3j_alphaL","top_PfMt3j_alphaR");
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+      if(opts.getBoolVal("smearTop4j")) {
+        MLStrList params("top_PfMt4j_mean","top_PfMt4j_sigmaL","top_PfMt4j_sigmaR","top_PfMt4j_alphaL","top_PfMt4j_alphaR");
+        theFit2[ifit]->uniformVaryConstantParameters(params);
+      }
+
+      if( myPdf2 !=0 ) theStudy.addFit(*myPdf2);
+    }
+  }
+
   theStudy.generateAndFit(nexp,ngen);
   
   char resultfilename[200];
   if (outfile==0) sprintf(resultfilename,"toys/output/resultsW-%s.dat",jetflavour);
-  else sprintf(resultfilename, outfile, njets);
+  else sprintf(resultfilename, outfile);
   theStudy._fitParData->write(resultfilename);
 
   char variablesfilename[200];
@@ -262,3 +325,168 @@ void Generate(Int_t nexp = 1, UInt_t iseed = 65539, char* outfile= 0) {
 
 }
 
+void getSecondFit(int ifit) {
+
+  MLFit *theIFit = new MLFit();
+  char fitname[200];
+  sprintf(fitname,"myFit%d",ifit);
+
+  // Various fit options...
+  MLOptions opts = GetDefaultOptions();
+
+  // define the structure of the dataset
+  RooRealVar* pfmt = new RooRealVar("pfmt",  "M_{T}" , 20., 150., "GeV/c^{2}");
+  RooRealVar* nbj = new RooRealVar("nBTagJets", "n_{b-tag jets}", 0.0, 3.0);
+  RooRealVar *njetsHi = new RooRealVar("nExclPFJetsHi", "nExclPFJetsHi",0.5, 20.5);
+
+  theIFit->AddFlatFileColumn(pfmt);
+  theIFit->AddFlatFileColumn(nbj);
+  theIFit->AddFlatFileColumn(njetsHi);
+
+  // define a fit model
+  theIFit->addModel(fitname, "Ratio WtoENu");
+
+  // define species
+  cout << "min = " << opts.getRealVal("njetmin") << " max = " << opts.getRealVal("njetmax") << endl;
+  MLStrList speclist;
+  for(int nj = opts.getRealVal("njetmin"); nj <=opts.getRealVal("njetmax"); nj++){
+    //signal (assumption is W is all 0 bjets in the acceptance)
+    char speclabel[50], specdesc[200];
+    sprintf(speclabel,"W_%dj",nj);
+    sprintf(specdesc,"Signal %dj Component",nj);
+    theIFit->addSpecies(fitname, speclabel, specdesc);
+    speclist.Add(speclabel); // exclusive species list ot be transformed in inclusive.
+
+    // top (ttbar + single t)
+    sprintf(speclabel,"top0b_%dj",nj); // 0 bjet
+    sprintf(specdesc,"Top background 0bjet %dj Component",nj);
+    theIFit->addSpecies(fitname, speclabel, specdesc);    
+
+    sprintf(speclabel,"top1b_%dj",nj); // 1 bjet
+    sprintf(specdesc,"Top background 1bjet %dj Component",nj);
+    theIFit->addSpecies(fitname, speclabel, specdesc);    
+
+    sprintf(speclabel,"top2b_%dj",nj); // 2 bjet
+    sprintf(specdesc,"Top background 2bjet %dj Component",nj);
+    theIFit->addSpecies(fitname, speclabel, specdesc);    
+
+    // Other
+    sprintf(speclabel,"other_%dj",nj);
+    sprintf(specdesc,"Other background %dj Component",nj);
+    theIFit->addSpecies(fitname, speclabel, specdesc);
+
+  }
+ 
+  if(opts.getBoolVal("fitRatio"))  {
+    theIFit->fitInclusiveRatio(speclist, "Wincl",opts.getRealVal("njetmin"));
+  }
+  else { 
+    if(opts.getBoolVal("fitInclusive")) theIFit->fitInclusive( speclist, "Wincl_",opts.getRealVal("njetmin"));
+  }
+
+  char speclabel[50],pdfname[200],speclabelToCopy[200];
+  for(int nj = opts.getRealVal("njetmin"); nj <=opts.getRealVal("njetmax"); nj++){
+    
+    // Mt PDF
+    if(opts.getBoolVal("usePfMt")) {
+      
+      //signal
+      sprintf(speclabel,"W_%dj",nj);
+      sprintf(pdfname,"sig_PfMt%dj",nj);   
+      theIFit->addPdfWName(fitname, speclabel , "pfmt",  "DoubleCruijff", pdfname );
+
+      //Other
+      sprintf(speclabel,"other_%dj",nj);
+      sprintf(pdfname,"other_PfMt%dj",nj);   
+      theIFit->addPdfWName(fitname,speclabel  , "pfmt",  "Cruijff",   pdfname);
+
+      //top
+      sprintf(speclabel,"top0b_%dj",nj);
+      sprintf(pdfname,"top_PfMt%dj",nj);   
+      theIFit->addPdfWName(fitname,speclabel  , "pfmt",  "Cruijff",   pdfname);
+      
+      sprintf(speclabel,"top1b_%dj",nj);
+      sprintf(speclabelToCopy,"top0b_%dj",nj);
+      theIFit->addPdfCopy(fitname, speclabel, "pfmt", speclabelToCopy);
+
+      sprintf(speclabel,"top2b_%dj",nj);
+      theIFit->addPdfCopy(fitname, speclabel, "pfmt", speclabelToCopy);
+    }
+
+    // b-tagged jet multiplicity
+    char effb0[50], effnob0[50], effb1[50], effnob1[50], effb2[50], effnob2[50];
+    if(opts.getBoolVal("useBTag")) {
+
+      // signal
+      sprintf(speclabel,"W_%dj",nj);
+      sprintf(pdfname,"b0_btag_%dj",nj);   // call W+0 bjets the signal (by definition)
+      //sprintf(pdfname,"sig_b0_btag_%dj",nj); 
+      theIFit->addPdfWName(fitname, speclabel , "nBTagJets",  "BtagPdf", pdfname );
+      
+      // Other
+      // floated on data. No need of measuring on control sample
+      const int nbins = 3;
+      double limitarray[] = {0.0, 1.0, 2.0, 3.0 };
+
+      TAxis* limits = new TAxis(nbins,limitarray) ;
+      TList args;
+      args.Add(limits);
+
+      sprintf(speclabel,"other_%dj",nj);
+      sprintf(pdfname,"other_btag_%dj",nj);
+      theIFit->addPdfWName(fitname, speclabel , "nBTagJets",  "BinnedPdf", args, pdfname );
+
+      // top
+      sprintf(speclabel,"top0b_%dj",nj);
+      sprintf(pdfname,"b0_btag_%dj",nj);
+      //sprintf(pdfname,"top_b0_btag_%dj",nj);
+      theIFit->addPdfWName(fitname, speclabel , "nBTagJets",  "BtagPdf", pdfname );
+
+      sprintf(speclabel,"top1b_%dj",nj);
+      sprintf(pdfname,"b1_btag_%dj",nj);
+      //sprintf(pdfname,"top_b1_btag_%dj",nj);
+      theIFit->addPdfWName(fitname, speclabel , "nBTagJets",  "BtagPdf", pdfname );
+
+      sprintf(speclabel,"top2b_%dj",nj);
+      sprintf(pdfname,"b2_btag_%dj",nj);
+      //sprintf(pdfname,"top_b2_btag_%dj",nj);
+      theIFit->addPdfWName(fitname, speclabel , "nBTagJets",  "BtagPdf", pdfname );
+
+      // tagging and mistag rate are jet properties: unique over jet multiplicities
+      sprintf(effb0,"b0_btag_%dj_eb",nj);
+      sprintf(effb1,"b1_btag_%dj_eb",nj);
+      sprintf(effb2,"b2_btag_%dj_eb",nj);
+      sprintf(effnob0,"b0_btag_%dj_enob",nj);
+      sprintf(effnob1,"b1_btag_%dj_enob",nj);
+      sprintf(effnob2,"b2_btag_%dj_enob",nj);
+
+      theIFit->bind(MLStrList(effb0,effb1,effb2),"btag_eb","btag_eb");
+      theIFit->bind(MLStrList(effnob0,effnob1,effnob2),"btag_enob","btag_enob");
+
+    }
+    
+  
+    // jet category
+    sprintf(pdfname,"%dj",nj);
+
+    sprintf(speclabel,"W_%dj",nj);
+    theIFit->addPdfWName(fitname,speclabel , "nExclPFJetsHi", "ExclJet", pdfname);
+
+    sprintf(speclabel,"other_%dj",nj);
+    theIFit->addPdfWName(fitname,speclabel , "nExclPFJetsHi", "ExclJet", pdfname);
+
+    sprintf(speclabel,"top0b_%dj",nj);
+    theIFit->addPdfWName(fitname,speclabel , "nExclPFJetsHi", "ExclJet", pdfname);
+
+    sprintf(speclabel,"top1b_%dj",nj);
+    theIFit->addPdfWName(fitname,speclabel , "nExclPFJetsHi", "ExclJet", pdfname);
+
+    sprintf(speclabel,"top2b_%dj",nj);
+    theIFit->addPdfWName(fitname,speclabel , "nExclPFJetsHi", "ExclJet", pdfname);
+
+  }
+
+  theIFit->Print();
+  theFit2[ifit] = theIFit;
+
+}
